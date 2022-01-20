c93957a1fe2076d5176c57aab8bfddd61c075de5
Anthony RYCK <anthony.ryck@hotmail.fr>
2022-01-20 06:19:57 +0100

    Pour reprendre ailleur

---
+++ src/RyckBlazor/GlobalUsing.cs
@@ -0,0 +1,3 @@
+﻿global using Microsoft.AspNetCore.Components;
+global using RyckBlazor;
+global using RyckBlazor.ViewModels;
---
+++ src/RyckBlazor/ViewModels/ILogsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace RyckBlazor.ViewModels
+{
+	public interface ILogsViewModel
+	{
+		List<LogEntity> Logs { get; }
+
+		DateTime DateDebutLog { get; set; }
+
+		void OnChangeLevel(ChangeEventArgs e);
+
+
+		Task LoadLogs();
+	}
+}
